4bc3b089ce31ae348408630fd5f71b3e27415d19
yanyi0 <785144130@qq.com>
2018-07-27 02:20:15 +0800

    Initial Commit

---
+++ TestPrivateCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPrivateCocoapods
+//
+//  Created by Cloud on 2018/7/27.
+//  Copyright © 2018年 Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPrivateCocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPrivateCocoapods
+//
+//  Created by Cloud on 2018/7/27.
+//  Copyright © 2018年 Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
